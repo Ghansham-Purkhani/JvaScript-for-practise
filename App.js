a0ec4f273d834343593bc7a050e5edb1aa921c00
Ghansham Purkhani <ghanshampurkhani79@gmail.com>
2025-05-19 14:50:27 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+const custId = 1234;
+let custEmail = "44303........37";
+var cusPassword = "1234";
+cusCity = "Mithi"; // without declearation you can add some value not prefferd
+// custId = 4; not allowed change value of constant varriable
+custEmail = "44@gmail.com";
+cusPassword = "121212";
+cusCity = "karachi";
+console.table([custId,cusPassword,custEmail,cusCity]);
+// console.log(custId);
